7326e0343535a11f236b15fa3fecc400ebb7ca76
Loyal Chow <loyalchow@gmail.com>
2017-07-20 20:27:45 -0700

    fix syntax

--- screener.config.js
+++ screener.config.js
@@ -3,7 +3,7 @@ module.exports = {
   projectRepo: 'loyalchow/screener-v2-test',
   failureExitCode: 0,
   branch: process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH,
-  commit: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT
+  commit: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT,
   states: [
     {
       url: 'https://screener.io',
